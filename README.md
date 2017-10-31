f7f36954d66d980096950c7b952e008ec4504044
Ienesca Roland <ienesca.roland@gmail.com>
2017-10-31 21:30:38 +0200

    view,edit,delete books & table pagination

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-# Programare Web - Bookshop
-
 -> optiune de logare/inregistrare
 
 Operatii ADMIN:
 
-  -> adaugare carte (setare pret/stock)
-  
-  -> stergere comentarii
+-> adaugare carte (setare pret/stock)
+
+-> stergere comentarii
 
 Operatii USER:
 
-  -> cumparare carte
-  
-  -> adaugare review/rating la carti cumparate
-  
-  -> adaugare comentariu la carti necumparate
-  
-  -> request carti
+-> cumparare carte
+
+-> adaugare review/rating la carti cumparate
+
+-> adaugare comentariu la carti necumparate
+
+-> request carti
--- src/AppBundle/Controller/BookshopController.php
+++ src/AppBundle/Controller/BookshopController.php
@@ -24,14 +24,24 @@ class BookshopController extends Controller
      /**
      * @Route("/books", name="bookshop_books")
      */
-    public function booksAction()
+    public function booksAction(Request $request)
     {   
+        $em = $this->getDoctrine()->getManager();
         $books = $this->getDoctrine()
             ->getRepository('AppBundle:Book')
             ->findAll();
+        $dql = "SELECT b FROM AppBundle:Book b";
+        $query = $em->createQuery($dql);
+
+        $paginator = $this->get('knp_paginator');
+        $result = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 10)
+        );
 
         return $this->render('bookshop/books.html.twig', array(
-            'books' => $books
+            'books' => $result
         ));
     }
 
@@ -76,10 +86,82 @@ class BookshopController extends Controller
     }
 
     /**
-     * @Route("/details/{id}", name="bookshop_details")
+     * @Route("/bookshop/details/{id}", name="bookshop_details")
      */
     public function detailsAction($id)
     {
-        return $this->render('bookshop/details.html.twig');  
+        $book = $this->getDoctrine()
+            ->getRepository('AppBundle:Book')
+            ->find($id);
+
+        return $this->render('bookshop/details.html.twig', array(
+            'book' => $book
+        ));
+    }
+
+    /**
+     * @Route("/bookshop/edit/{id}", name="bookshop_edit")
+     */
+    public function editAction($id, Request $request)
+    {
+        $book = $this->getDoctrine()
+            ->getRepository('AppBundle:Book')
+            ->find($id);
+
+        $book->setTitle($book->getTitle());
+        $book->setAuthor($book->getAuthor());
+        $book->setPrice($book->getPrice());
+
+        $form = $this->createFormBuilder($book)
+            ->add('title', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('author', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('price', NumberType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('submit', SubmitType::class, array('label' => 'Edit Book', 'attr' => array('class' => 'btn btn-success', 'style' => 'margin-bottom:15px')))
+            ->getForm();
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $title = $form['title']->getData();
+            $author = $form['author']->getData();
+            $price = $form['price']->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $book = $em->getRepository('AppBundle:Book')->find($id);
+            
+            $book->setTitle($title);
+            $book->setAuthor($author);
+            $book->setPrice($price);
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Book Updated'
+            );
+            return $this->redirectToRoute('bookshop_books');
+        }
+
+        return $this->render('bookshop/edit.html.twig', array(
+            'book' => $book,
+            'form' => $form->createView()
+        ));
+    }
+
+     /**
+     * @Route("/bookshop/delete/{id}", name="bookshop_delete")
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $book = $em->getRepository('AppBundle:Book')->find($id);
+
+        $em->remove($book);
+        $em->flush();
+        $this->addFlash(
+            'notice',
+            'Book Removed'
+        );
+
+        return $this->redirectToRoute('bookshop_books');
     }
 }
